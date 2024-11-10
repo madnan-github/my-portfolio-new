d2f2d6ec761a9e300b179da19159b2b66558dd52
madnan-github <dononlineboss@gmail.com>
2024-11-10 23:33:57 +0500

    update single quote

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 const abouts = [
   { title: "Professional Expertise", content: "I specialize in web development using HTML, CSS, JavaScript & TypeScript and modern frameworks like Next.js. My focus is on creating sleek, responsive interfaces that deliver a smooth user experience across all devices."},
   { title: "Design Meets Development", content: "I have a deep appreciation for both design and functionality. I enjoy the process of turning creative ideas into fully-functional products, ensuring they are not only visually appealing but also easy to navigate for users."},
-  { title: "Performance Optimization", content: "I believe that speed is critical to good user experience. That&apos;s why I focus on optimizing load times and interactions, ensuring that my projects run efficiently and smoothly, no matter the complexity."},
+  { title: "Performance Optimization", content: "I believe that speed is critical to good user experience. That&#39;s why I focus on optimizing load times and interactions, ensuring that my projects run efficiently and smoothly, no matter the complexity."},
   { title: "Lifelong Learner", content: "Technology evolves rapidly, and I enjoy keeping up with the latest tools, frameworks, and industry trends. Continuous learning is a core part of my approach, helping me improve my skills and bring innovative solutions to the table."},
   { title:"Team Player and Collaborator", content: "My experience working closely with designers, developers, and project stakeholders has taught me the value of communication and teamwork. I thrive in collaborative environments and enjoy contributing to group success."},
   ]
